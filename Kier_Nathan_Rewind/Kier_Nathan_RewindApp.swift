a07ee6f22439176bae55eecff01b31a3a08f9ea3
NKIER200 <NKIER200@Caledonian.ac.uk>
2022-06-14 20:06:45 +0100

    Initial Commit

---
+++ Kier_Nathan_Rewind/Kier_Nathan_RewindApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kier_Nathan_RewindApp.swift
+//  Kier_Nathan_Rewind
+//
+//  Created by Nathan Kier on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Kier_Nathan_RewindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
